dac104c563d69d969f7141c0f9d274feeb27b601
AhmadMujtaba25 <ahmadw6969@gmail.com>
2025-04-27 20:33:57 +0500

    All Tsk Completed

---
+++ main.py
@@ -0,0 +1,14 @@
+from file_handling_tasks import *
+
+task1_create_file()
+task2_read_file()
+task3_append_file()
+task2_read_file()
+task4_count_lines()
+task5_find_word()
+task6_copy_file()
+task7_replace_word()
+task2_read_file()
+task9_write_csv()
+task8_read_csv()
+task10_json_file() 
