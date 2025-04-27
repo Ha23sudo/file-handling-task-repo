a836eaf4f7d5ac6de69019c5ed6d8cdf1bd26a4c
Aayan farooq <Farooqaayan2@gmail.com>
2025-04-28 01:01:35 +0500

    All tasks done

--- file_handling_tasks.py
+++ file_handling_tasks.py
@@ -79,15 +79,86 @@ def task7_replace_word():
     pass
 def task8_read_csv():
     # TODO: Read a CSV file and print each row.
+    #CSV file is a simple text file that has values seprated by commas for better readability, sort of a excel sheet
+    import csv
+    f=open(r"C:\\Users\\hp\\OneDrive\\Desktop\\4th Semester\\Python\\taskfile.csv.txt", 'r')
+    values=csv.reader(f)
+    for row in values: 
+        print(row)
     pass
 
 def task9_write_csv():
     # TODO: Write a list of dictionaries to a CSV file.
-    pass
+    #create a dictionary , it should have keys and some values  
+ data = [
+    {'Name': 'Aayan', 'Age': '21', 'GPA': '3.4'},
+    {'Name': 'Ali', 'Age': '20', 'GPA': '3.0'},
+    {'Name': 'Farhan', 'Age': '21', 'GPA': '3.5'},
+    {'Name': 'Hamza', 'Age': '22', 'GPA': '2.9'}
+]
+# Open a file for writing
+ f = open(r"C:\Users\hp\OneDrive\Desktop\4th Semester\Python\taskfile.csv.txt", 'w')
+ f.write('Name,Age,GPA\n') #for headings
+# Write each students data
+ for row in data:
+    line = f"{row['Name']},{row['Age']},{row['GPA']}\n"
+    f.write(line)
+ f.close()
+pass
 
 def task10_json_file():
     # TODO: Create a JSON file from a Python dictionary and read it back.
+    import json
+    data = {
+    "Name": "Aayan",
+    "Age": 21,
+    "GPA": 3.4
+    }
+    f=open(r"C:\Users\hp\OneDrive\Desktop\4th Semester\Python\task10.json", 'w')
+    json.dump(data,f) #this creates a file from python and adds data to it using dump command
+    f.close()
+    f=open(r"C:\Users\hp\OneDrive\Desktop\4th Semester\Python\task10.json", 'r')
+    newdata=json.load(f)
+    print(newdata)
     pass
 
 #add functions here properly for a menu based program
-task7_replace_word()
+x=0
+while x!=1:
+    print("\n      WELCOME TO PYTHON FILE HANDLING SIMULATOR \n")
+    print("<---------------- HERE IS A MENU OF WHAT YOU CAN DO IN THIS SIMULATOR ------------>")
+    print("Press 1 :       TO  create a new text file and write Hello, world! to it.")
+    print("Press 2 :       TO  read the contents of a file and print them to the console.")
+    print("Press 3 :       TO  append a new line of text to an existing file.")
+    print("Press 4 :       TO  count and print the number of lines in a file.")
+    print("Press 5 :       TO  find whether a specific word exists in the file and how many times.")
+    print("Press 6 :       TO  copy the contents of one file to another.")
+    print("Press 7 :       TO  replace a specific word in the file with another word.")
+    print("Press 8 :       TO  read a CSV file and print each row.")
+    print("Press 9 :       TO  write a list of dictionaries to a CSV file.")
+    print("Press 10 :      TO  create a JSON file from a Python dictionary and read it back.")
+    print("Press 11 ******************** TO EXIT THE PROGRAM ******************************")
+    choice=int(input("Enter your choice :"))
+    if choice ==1:
+        task1_create_file()
+    elif choice==2:
+        task2_read_file()
+    elif choice==3:
+        task3_append_file()
+    elif choice==4:
+        task4_count_lines()
+    elif choice==5:
+        task5_find_word()
+    elif choice==6:
+        task6_copy_file()
+    elif choice==7:
+        task7_replace_word()
+    elif choice==8:
+        task8_read_csv()
+    elif choice==9:
+        task9_write_csv()
+    elif choice==10:
+        task10_json_file()
+    else:
+        print("------> THANK YOU FOR USING THIS SIMULATOR , SEEE YOU NEXT TIME :) ")
+        x=1
