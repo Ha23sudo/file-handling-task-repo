d6aec74af1e753234f318201da8b7d8524ae6175
zainab <zainab@gmail.com>
2025-04-28 18:35:33 +0500

    task 1 done

--- file_handling_tasks.py
+++ file_handling_tasks.py
@@ -5,30 +5,61 @@
 
 def task1_create_file():
     # TODO: Create a new text file and write "Hello, world!" to it.
+    file=open("file.txt",'w')
+    file.write("Hello, World!")
+    file.close()
     pass
 
 def task2_read_file():
     # TODO: Read the contents of a file and print them to the console.
+    file=open("file.txt",'r')
+    content=file.read()
+    print(content)
+    file.close()
     pass
 
 def task3_append_file():
     # TODO: Append a new line of text to an existing file.
+    file=open("file.txt",'a')
+    file.write(" How are you people")
+    file.close()
     pass
 
 def task4_count_lines():
     # TODO: Count and print the number of lines in a file.
+    file=open("file.txt",'r')
+    content=file.read()
+    lines=content.count('\n')
+    print(lines+1)
+    file.close()
     pass
 
 def task5_find_word():
     # TODO: Find whether a specific word exists in the file and how many times.
+    file=open("file.txt",'r')
+    content=file.read()
+    word=content.count('\n')
+    if word != 0:
+        print("yes ,,,",word)
+    else:
+        print("no")    
+    file.close()
     pass
 
 def task6_copy_file():
     # TODO: Copy the contents of one file to another.
+    file=open("file.txt",'r')
+    file2=open("file2.txt",'w')
+    for i in file:
+        file2.write(i)
+    file.close() 
+    file2.close()   
     pass
 
 def task7_replace_word():
     # TODO: Replace a specific word in the file with another word.
+    file=open("file.txt",'r')
+     
     pass
 
 def task8_read_csv():
@@ -42,3 +73,10 @@ def task9_write_csv():
 def task10_json_file():
     # TODO: Create a JSON file from a Python dictionary and read it back.
     pass
+
+#task1_create_file()
+#task2_read_file()
+#task3_append_file()
+#task4_count_lines()
+#task5_find_word()
+#task6_copy_file()
